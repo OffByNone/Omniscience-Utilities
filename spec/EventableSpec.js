410c0ba7a4b12081b7a78e99756335d02ebbe689
OffByNone <jeremy.mcneil@gmail.com>
2015-06-24 22:40:00 -0500

    Added configs for eslint and fixed eslint errors
    Also now validate the params in eventable

--- spec/EventableSpec.js
+++ spec/EventableSpec.js
@@ -1,33 +1,77 @@
 ///<reference path="./support/jasmine.d.ts" />
+/*eslint prefer-const:0*/
+"use strict";
+
 require("babel/register");
+const Constants = require("../lib/Constants");
 const Eventable = require("../lib/Eventable");
 
 describe("Eventable", function () {
 	describe("on/emit", function () {
-		it("should call all callbacks bound using on and pass in each parameter separately, when calling emit using same eventType", function () { 
+		it("should call all callbacks bound using on and pass in each parameter separately, when calling emit using same eventType", function () {
 			//arrange
-			var sut = new Eventable();
-			var eventType = "testEvent";
+			let sut = new Eventable();
+			let eventType = "testEvent";
 
-			var callback1 = jasmine.createSpy("firstcallback");
-			var callback2 = jasmine.createSpy("secondcallback");
-			var callback3 = jasmine.createSpy("thirdcallback");
+			let callback1 = jasmine.createSpy("firstcallback");
+			let callback2 = jasmine.createSpy("secondcallback");
+			let callback3 = jasmine.createSpy("thirdcallback");
 
-			var arg1 = "arg1";
-			var arg2 = "arg2";
-			var arg3 = "arg3";
+			let arg1 = "arg1";
+			let arg2 = "arg2";
+			let arg3 = "arg3";
 
 			sut.on(eventType, callback1);
 			sut.on(eventType, callback2);
 			sut.on("some other event", callback3);
-			
+
 			//act
 			sut.emit(eventType, arg1, arg2, arg3);
-			
+
 			//assert
 			expect(callback1).toHaveBeenCalledWith(arg1, arg2, arg3);
 			expect(callback2).toHaveBeenCalledWith(arg1, arg2, arg3);
 			expect(callback3).not.toHaveBeenCalled();
+		});
+	});
+	describe("on", function () {
+		it("should throw error when eventType is null", function () {
+			//arrange
+			let sut = new Eventable();
+
+			try {
+				sut.on(null, null);
+				fail("expected error was not thrown");
+			}
+			catch (e) {
+				expect(e.message).toBe(Constants.argumentNullError + "eventType");
+			}
+		});
+		it("should throw error when callback is not a function", function () {
+			//arrange
+			let sut = new Eventable();
+
+			try {
+				sut.on("someevent", null);
+				fail("expected error was not thrown");
+			}
+			catch (e) {
+				expect(e.message).toBe(Constants.argumentTypeError + "callback must be a function.");
+			}
+		});
+	});
+	describe("emit", function () {
+		it("should throw error when eventType is null", function () {
+			//arrange
+			let sut = new Eventable();
+
+			try {
+				sut.on(null, null);
+				fail("expected error was not thrown");
+			}
+			catch (e) {
+				expect(e.message).toBe(Constants.argumentNullError + "eventType");
+			}
 		});
 	});
 });
--- spec/UrlProviderSpec.js
+++ spec/UrlProviderSpec.js
@@ -1,11 +1,14 @@
 ///<reference path="./support/jasmine.d.ts" />
+/*eslint prefer-const:0*/
+"use strict";
+
 require("babel/register");
 const Constants = require("../lib/Constants");
 const UrlProvider = require("../lib/UrlProvider");
 
 describe("UrlProvider", function () {
-	var _sut;
-	var _mockSdk = {};
+	let _sut;
+	const _mockSdk = {};
 	beforeEach(function () {
 		_sut = new UrlProvider(_mockSdk);
 	});
@@ -13,43 +16,43 @@ describe("UrlProvider", function () {
 	describe("toUrl", function () {
 		it("should return null when path is null", function () {
 			//arrange
-			var path = null;
+			let path = null;
 
 			//act
-			var actual = _sut.toUrl(path, null, null);
+			let actual = _sut.toUrl(path, null, null);
 
 			//assert
 			expect(actual).toBeNull();
 		});
 		it("should return null when path is an empty string", function () {
 			//arrange
-			var path = "";
+			let path = "";
 
 			//act
-			var actual = _sut.toUrl(path, null, null);
+			let actual = _sut.toUrl(path, null, null);
 
 			//assert
 			expect(actual).toBeNull();
 		});
 		it("should return null when path is not found", function () {
 			//arrange
-			var path = Constants.notFound;
+			let path = Constants.notFound;
 
 			//act
-			var actual = _sut.toUrl(path, null, null);
+			let actual = _sut.toUrl(path, null, null);
 
 			//assert
 			expect(actual).toBeNull();
 		});
 		it("should return path as url object when path is a valid uri", function () {
 			//arrange
-			var path = "http://example.com";
-			var expected = { url: "your new url" };
-			_mockSdk.isValidURI = jasmine.createSpy("valid url spy").and.returnValue(true)
+			let path = "http://example.com";
+			let expected = { url: "your new url" };
+			_mockSdk.isValidURI = jasmine.createSpy("valid url spy").and.returnValue(true);
 			_mockSdk.URL = jasmine.createSpy("new url spy").and.returnValue(expected);
 
 			//act
-			var actual = _sut.toUrl(path, null, null);
+			let actual = _sut.toUrl(path, null, null);
 
 			//assert
 			expect(_mockSdk.URL).toHaveBeenCalledWith(path, undefined);
@@ -57,15 +60,15 @@ describe("UrlProvider", function () {
 		});
 		it("should return baseUrl + path as url object when path is not a valid uri, and baseUrl + path is.", function () {
 			//arrange
-			var path = "/test";
-			var baseUrl = "http://example.com";
-			var currentUrl = "http://example.com/testing/2";
-			var expected = { url: "your new url" };
-			_mockSdk.isValidURI = jasmine.createSpy("valid url spy").and.returnValue(false)
+			let path = "/test";
+			let baseUrl = "http://example.com";
+			let currentUrl = "http://example.com/testing/2";
+			let expected = { url: "your new url" };
+			_mockSdk.isValidURI = jasmine.createSpy("valid url spy").and.returnValue(false);
 			_mockSdk.URL = jasmine.createSpy("new url spy").and.returnValue(expected);
 
 			//act
-			var actual = _sut.toUrl(path, currentUrl, baseUrl);
+			let actual = _sut.toUrl(path, currentUrl, baseUrl);
 
 			//assert
 			expect(_mockSdk.URL).toHaveBeenCalledWith(path, baseUrl);
@@ -73,11 +76,11 @@ describe("UrlProvider", function () {
 		});
 		it("should return currentUrl.origin + path as url object when path is not valid, baseUrl + path is not valid, and currentUrl.origin + path is.", function () {
 			//arrange
-			var path = "/test";
-			var baseUrl = "";
-			var currentUrl = "http://example.com/testing/2";
-			var expected = { url: "your new url" };
-			_mockSdk.isValidURI = jasmine.createSpy("valid url spy").and.returnValue(false)
+			let path = "/test";
+			let baseUrl = "";
+			let currentUrl = "http://example.com/testing/2";
+			let expected = { url: "your new url" };
+			_mockSdk.isValidURI = jasmine.createSpy("valid url spy").and.returnValue(false);
 			_mockSdk.URL = jasmine.createSpy("new url spy").and.callFake(function (source, base) {
 				if (base === baseUrl)
 					throw new Error("error");
@@ -85,7 +88,7 @@ describe("UrlProvider", function () {
 			});
 
 			//act
-			var actual = _sut.toUrl(path, currentUrl, baseUrl);
+			let actual = _sut.toUrl(path, currentUrl, baseUrl);
 
 			//assert
 			expect(_mockSdk.URL).toHaveBeenCalledWith(path, baseUrl);
@@ -96,14 +99,14 @@ describe("UrlProvider", function () {
 	describe("createUrl", function () {
 		it("should return a new URL object", function () {
 			//arrange
-			var baseUrl = "http://example.com";
-			var source = "http://example.com/testing/2";
-			var expected = { url: "your new url" };
+			let baseUrl = "http://example.com";
+			let source = "http://example.com/testing/2";
+			let expected = { url: "your new url" };
 
 			_mockSdk.URL = jasmine.createSpy("new url spy").and.returnValue(expected);
 
 			//act
-			var actual = _sut.createUrl(source, baseUrl);
+			let actual = _sut.createUrl(source, baseUrl);
 
 			//assert
 			expect(_mockSdk.URL).toHaveBeenCalledWith(source, baseUrl);
@@ -114,9 +117,9 @@ describe("UrlProvider", function () {
 		it("should return true when Uri is valid", function () {
 			//arrange
 			_mockSdk.isValidURI = jasmine.createSpy("isValidURI").and.returnValue(true);
-			var uri = "a valid url here";
+			let uri = "a valid url here";
 			//act
-			var actual = _sut.isValidUri(uri);
+			let actual = _sut.isValidUri(uri);
 			//assert
 			expect(_mockSdk.isValidURI).toHaveBeenCalledWith(uri);
 			expect(actual).toBeTruthy();
@@ -124,9 +127,9 @@ describe("UrlProvider", function () {
 		it("should return false when Uri is not valid", function () {
 			//arrange
 			_mockSdk.isValidURI = jasmine.createSpy("isValidURI").and.returnValue(false);
-			var uri = "a non valid url here";
+			let uri = "a non valid url here";
 			//act
-			var actual = _sut.isValidUri(uri);
+			let actual = _sut.isValidUri(uri);
 			//assert
 			expect(_mockSdk.isValidURI).toHaveBeenCalledWith(uri);
 			expect(actual).toBeFalsy();
