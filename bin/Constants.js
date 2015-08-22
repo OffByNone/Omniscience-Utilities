7c06bb0fedcf1c6ec2f9d31a7742327781d79e23
OffByNone <jeremy.mcneil@gmail.com>
2015-08-22 15:55:24 -0500

    last checkin simply ignored the bin, this one removes it.

--- bin/Constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-module.exports.notFound = "/ssdp/notfound"; //for matchstick devices and maybe chromecast
-module.exports.argumentNullError = "Argument Null Error.  Argument Name: ";
-module.exports.argumentTypeError = "Argument Type Error.  Argument Name: ";
